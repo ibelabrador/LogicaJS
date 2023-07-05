bb9873ee92fd8f7c2feba5c68eb8c93e1cb7390f
Isabel C Labrador V <isabelclabradorv@MacBook-Pro-de-Leidy.local>
2023-07-04 20:14:47 -0500

    Avance nuevos ejercicios

--- ejercicios/ejercicios.md
+++ ejercicios/ejercicios.md
@@ -9,3 +9,16 @@
 08. Escribir una función que reciba número entero de dos dígitos y determine si ambos dígitos son pares.
 09. Escribir una función que reciba número entero de dos dígitos menor que 20 y determine si es primo.
 10. Escribir una función que reciba número entero de dos dígitos y determine si es primo y además si es negativo.
+
+## Ejercicios Ciclos
+
+01. Modificar el ejercicio pasado para encontrar un número primo para que se soluciones con ciclos:
+02. Escriba una función que reciba un número entero positivo y mostrar todos los enteros comprendidos entre 1 y el número leído.
+03. Escriba una función que reciba un número entero positivo y mostrar todos los pares comprendidos entre 1 y el número leído.
+04. Escriba una función que reciba un número entero positivo y mostrar todos los divisores exactos del número comprendidos entre 1 y el número leído.
+05. Escriba una función que reciba dos números y mostrar todos los enteros comprendidos entre ellos.
+06. Escriba una función que reciba dos números y mostrar todos los números terminados en 4 comprendidos entre ellos.
+07. Escriba una función que reciba un número entero de tres dígitos y mostrar todos los enteros comprendidos entre 1 y cada uno de los dígitos.
+08. Escriba una función que muestre en consola todos los enteros comprendidos entre 1 y 100.
+09. Escriba una función que muestre en consola todos los pares comprendidos entre 20 y 200.
+10. Escriba una función que muestre en consola en pantalla todos los números terminados en 6 comprendidos entre 25 y 205.
--- ejercicios/soluciones.js
+++ ejercicios/soluciones.js
@@ -66,14 +66,95 @@ function digitosPar(num){
     }
 }   
 
-function numPrimo(num){
-    if(num>9 && num<20){
-        if(!num%2===0){
-            return "Es primo"
-        }else {
-        return "No es primo"
+function esPrimo(num) {
+    if(num > 9 && num <= 20) {
+        var noPrimo = "El numero no es primo"
+        if (num > 2 && num % 2 == 0) {
+            return noPrimo
         }
-    }else {
-        return "No es un número de dos dígitos menor que 20"
+        if (num > 3 && num % 3 === 0) {
+            return noPrimo
+        }
+        if (num > 5 && num % 5 === 0) {
+            return noPrimo
+        }
+        if (num > 7 && num % 7 === 0) {
+            return noPrimo
+        }
+        if (num > 11 && num % 11 === 0) {
+            return noPrimo
+        }
+        if (num > 13 && num % 13 === 0) {
+            return noPrimo
+        }
+        if (num > 17 && num % 17 === 0) {
+            return noPrimo
+        }
+        if (num > 19 && num % 19 === 0) {
+            return noPrimo
+        }
+        return "El numero es primo"
+    } else {
+        return "No es un numero valido";
     }
+}
+
+const esPrimo = (num) => {
+    if(num > 9 && num <= 20) {
+        for(let i=2; i<num; i++) {
+            if(num % i === 0) {
+                return "El numero no es primo"
+            }
+        }
+        return "El numero es primo"
+    }
+    return "No es un numero valido";
+}
+
+const esPrimo2 = (num) => {
+    if(!(num > 9 && num <= 20)) {
+        return "No es un numero valido";
+    }
+    for(let i=2; i<num; i++) {
+        if(num % i === 0) {
+            return "El numero no es primo"
+        }
+    }
+    return "El numero es primo"
+}
+
+const numPositivo = (num) => {
+    var listaNumeros = []
+    for(let i=1; i<=num; i++) {
+        listaNumeros.push(i)
+    }
+    return listaNumeros
+}
+
+const numPares = (num) => {
+    var listaPares = []
+    for(let i=2; i<=num; i+=2){
+        listaPares.push(i)
+    }
+    return listaPares
+}
+
+const divisoresExactos = (num) => {
+    var divisores = []
+    for(let i=1; i<=num; i++) {
+        if(num % i === 0) {
+            divisores.push(i)
+        }
+    }
+    return divisores
+}
+
+const dosNum = (num1, num2) => {
+    var listaEnteros = []
+    if(num1>num2){
+        for(let i=num1; i<=num2; i++){
+            listaEnteros.push(i)
+        }
+    }    
+    return listaEnteros
 }
